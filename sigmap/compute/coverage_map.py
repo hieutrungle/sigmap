5f390f1ed2514f11b269e298dd93a8f0d13457ef
hieule <hieult1996@gmail.com>
2024-02-01 13:27:45 -0600

    add index to mitsuba filename

--- sigmap/compute/coverage_map.py
+++ sigmap/compute/coverage_map.py
@@ -5,7 +5,7 @@ import os
 @timer.Timer(text="Elapsed coverage map time: {:0.4f} seconds\n", logger_fn=logger.log)
 def compute_coverage_map(args, config):
     # Prepare folders
-    cm_scene_folders, viz_scene_folders = utils.get_input_folders(config)
+    cm_scene_folders, viz_scene_folders = utils.get_input_folders(args, config)
     img_folder, img_tmp_folder, video_folder = utils.get_output_folders(config)
 
     # Compute coverage maps
@@ -33,23 +33,27 @@ def compute_coverage_map(args, config):
         filename = os.path.join(viz_scene_folder, f"{config.mitsuba_filename}.xml")
         scene = map_prep.prepare_scene(config, filename, cam)
 
+        render_filename = utils.create_filename(
+            img_tmp_folder, f"{config.mitsuba_filename}_{i:03d}.png"
+        )
         render_config = dict(
             camera=cam,
             paths=paths,
-            filename=os.path.join(
-                img_tmp_folder, f"{config.mitsuba_filename}_{i:02d}.png"
-            ),
+            # filename=os.path.join(
+            #     img_tmp_folder, f"{config.mitsuba_filename}_{i:02d}.png"
+            # ),
+            filename=render_filename,
             coverage_map=cm,
             cm_vmin=config.cm_vmin,
             cm_vmax=config.cm_vmax,
             resolution=config.resolution,
-            show_devices=False,
+            show_devices=True,
         )
         scene.render_to_file(**render_config)
 
-        if i == 0:
+        if i == 0 and i == len(cm_scene_folders) - 1:
             render_config["filename"] = os.path.join(
-                img_folder, f"{config.scene_name}_scene_{i:02d}.png"
+                img_folder, f"{config.scene_name}_scene_{i:03d}.png"
             )
-            render_config["show_devices"] = True
+            # render_config["show_devices"] = True
             scene.render_to_file(**render_config)
