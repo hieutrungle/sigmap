8b115ad6ff4b347c7d47e0e0758a5695b691b9f6
hieule <hieult1996@gmail.com>
2024-02-27 11:30:46 -0600

    remove old code

--- sigmap/blender_script/hallway.py
+++ sigmap/blender_script/hallway.py
@@ -15,53 +15,6 @@ import bl_utils, bl_parser
 
 
 def export_beamfocusing_simple_hallway(args, config):
-    # # Collection
-    # tiles = bpy.data.collections["Reflector"].objects
-
-    # for frame in range(1, 2):
-    #     for tile in tiles:
-    #         # local_bbox_center = 0.125 * sum(
-    #         #     (Vector(b) for b in tile.bound_box), Vector()
-    #         # )
-    #         # global_bbox_center = tile.matrix_world @ local_bbox_center
-    #         global_bbox_center = bl_utils.get_center_bbox(tile)
-
-    #         # Compute rotation angles
-    #         r, theta, phi = bl_utils.compute_rot_angle_3pts(
-    #             config.tx_position, global_bbox_center, config.rx_position
-    #         )
-
-    #         tile.rotation_euler = [0, theta, phi]
-    #         tile.scale = [0.1, 0.1, 0.02]
-
-    #     # Saving to mitsuba format for Sionna
-    #     print(
-    #         f"\nsaving with index: {args.index},  rx_pos: {config.rx_position} and tx_pos: {config.tx_position}"
-    #     )
-
-    #     # Save files without ceiling
-    #     folder_dir = os.path.join(
-    #         args.output_dir,
-    #         f"{config.scene_name}",
-    #         f"idx_{args.index}_rx_pos_{config.rx_position}",
-    #     )
-    #     bl_utils.mkdir_with_replacement(folder_dir)
-    #     bl_utils.save_mitsuba_xml(
-    #         folder_dir, config.mitsuba_filename, ["Reflector", "Wall", "Floor"]
-    #     )
-
-    #     # Save files with ceiling
-    #     folder_dir = os.path.join(
-    #         args.output_dir,
-    #         f"{config.scene_name}",
-    #         f"ceiling_idx_{args.index}_rx_pos_{config.rx_position}",
-    #     )
-    #     bl_utils.mkdir_with_replacement(folder_dir)
-    #     bl_utils.save_mitsuba_xml(
-    #         folder_dir,
-    #         config.mitsuba_filename,
-    #         ["Reflector", "Wall", "Floor", "Ceiling"],
-    #     )
 
     devices = []
     devices_names = []
