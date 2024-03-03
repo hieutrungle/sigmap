8182d902d515183571f5e81965d614c91daba9e0
hieule <hieult1996@gmail.com>
2024-03-03 11:33:49 -0600

    combine tee and simple hallway to beamfocusing

--- sigmap/blender_script/hallway.py
+++ sigmap/blender_script/hallway.py
@@ -14,66 +14,7 @@ if cmd_folder not in sys.path:
 import bl_utils, bl_parser
 
 
-def export_beamfocusing_simple_hallway(args, config):
-
-    devices = []
-    devices_names = []
-    for k, v in bpy.data.collections.items():
-        if "Reflector" in k:
-            devices_names.append(k)
-            devices.append(v.objects)
-
-    for tile_tuple in zip(*devices):
-
-        # [tx_pos, tile_center_1, tile_center_2, ..., rx_pos]
-        global_bbox_centers = []
-        for i, tile in enumerate(tile_tuple):
-            if i == 0:
-                global_bbox_centers.append(config.tx_position)
-            global_bbox_centers.append(bl_utils.get_center_bbox(tile))
-            if i == len(tile_tuple) - 1:
-                global_bbox_centers.append(config.rx_position)
-
-        for i in range(len(tile_tuple)):
-            r, theta, phi = bl_utils.compute_rot_angle_3pts(
-                global_bbox_centers[i],
-                global_bbox_centers[i + 1],
-                global_bbox_centers[i + 2],
-            )
-            tile_tuple[i].rotation_euler = [0, theta, phi]
-            tile_tuple[i].scale = [0.1, 0.1, 0.01]
-
-    # Saving to mitsuba format for Sionna
-    print(
-        f"\nsaving with index: {args.index},  rx_pos: {config.rx_position} and tx_pos: {config.tx_position}"
-    )
-
-    # Save files without ceiling
-    folder_dir = os.path.join(
-        args.output_dir,
-        f"{config.scene_name}",
-        f"idx_{args.index}_rx_pos_{config.rx_position}",
-    )
-    bl_utils.mkdir_with_replacement(folder_dir)
-    bl_utils.save_mitsuba_xml(
-        folder_dir, config.mitsuba_filename, [*devices_names, "Wall", "Floor"]
-    )
-
-    # Save files with ceiling
-    folder_dir = os.path.join(
-        args.output_dir,
-        f"{config.scene_name}",
-        f"ceiling_idx_{args.index}_rx_pos_{config.rx_position}",
-    )
-    bl_utils.mkdir_with_replacement(folder_dir)
-    bl_utils.save_mitsuba_xml(
-        folder_dir,
-        config.mitsuba_filename,
-        [*devices_names, "Wall", "Floor", "Ceiling"],
-    )
-
-
-def export_beamfocusing_tee_hallway(args, config):
+def export_beamfocusing_hallway(args, config):
 
     devices = []
     devices_names = []
@@ -227,10 +168,14 @@ def main():
     args = create_argparser().parse_args()
     config = bl_utils.make_conf(args.config_file)
 
-    if config.scene_name == "beamfocusing_simple_hallway":
-        export_beamfocusing_simple_hallway(args, config)
-    elif config.scene_name == "beamfocusing_tee_hallway":
-        export_beamfocusing_tee_hallway(args, config)
+    if "beamfocusing" in config.scene_name:
+        export_beamfocusing_hallway(args, config)
+
+    # if config.scene_name == "beamfocusing_simple_hallway":
+    #     export_beamfocusing_simple_hallway(args, config)
+    # elif config.scene_name == "beamfocusing_tee_hallway":
+    #     export_beamfocusing_tee_hallway(args, config)
+
     else:
         raise ValueError(f"Unknown scene name: {config.scene_name}")
 
