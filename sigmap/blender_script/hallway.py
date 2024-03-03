9a94e81c280580f1925fb9d3f6775bf3a4200269
Hieu Trung Le <hieult@tamu.edu>
2024-03-03 09:09:39 -0600

    export simple and no reflector hallway

--- sigmap/blender_script/hallway.py
+++ sigmap/blender_script/hallway.py
@@ -130,6 +130,99 @@ def export_beamfocusing_tee_hallway(args, config):
     )
 
 
+def export_no_reflector_scene(args, config):
+    # Saving to mitsuba format for Sionna
+    print(
+        f"\nsaving with index: {args.index},  rx_pos: {config.rx_position} and tx_pos: {config.tx_position}"
+    )
+
+    # Save files without ceiling
+    folder_dir = os.path.join(
+        args.output_dir,
+        f"{config.scene_name}",
+        f"idx_{args.index}_rx_pos_{config.rx_position}",
+    )
+    bl_utils.mkdir_with_replacement(folder_dir)
+    bl_utils.save_mitsuba_xml(folder_dir, config.mitsuba_filename, ["Wall", "Floor"])
+
+    # Save files with ceiling
+    folder_dir = os.path.join(
+        args.output_dir,
+        f"{config.scene_name}",
+        f"ceiling_idx_{args.index}_rx_pos_{config.rx_position}",
+    )
+    bl_utils.mkdir_with_replacement(folder_dir)
+    bl_utils.save_mitsuba_xml(
+        folder_dir,
+        config.mitsuba_filename,
+        ["Wall", "Floor", "Ceiling"],
+    )
+
+
+def export_flat_reflector_scene(args, config):
+    devices = []
+    devices_names = []
+    for k, v in bpy.data.collections.items():
+        if "Reflector" in k:
+            devices_names.append(k)
+            devices.append(v.objects)
+
+    rs, thetas, phis = [], [], []
+    for idx, tile_tuple in enumerate(zip(*devices)):
+        if idx == len(devices[0]) // 2:
+            global_bbox_centers = []
+            for i, tile in enumerate(tile_tuple):
+                if i == 0:
+                    global_bbox_centers.append(config.tx_position)
+                global_bbox_centers.append(bl_utils.get_center_bbox(tile))
+                if i == len(tile_tuple) - 1:
+                    global_bbox_centers.append(config.rx_position)
+            for i in range(len(tile_tuple)):
+                r, theta, phi = bl_utils.compute_rot_angle_3pts(
+                    global_bbox_centers[i],
+                    global_bbox_centers[i + 1],
+                    global_bbox_centers[i + 2],
+                )
+                rs.append(r)
+                thetas.append(theta)
+                phis.append(phi)
+            break
+
+    for idx, tile_tuple in enumerate(zip(*devices)):
+        for i in range(len(tile_tuple)):
+            tile_tuple[i].rotation_euler = [0, math.radians(90), math.radians(-45)]
+            tile_tuple[i].scale = [0.1, 0.1, 0.01]
+
+    # Saving to mitsuba format for Sionna
+    print(
+        f"\nsaving with index: {args.index},  rx_pos: {config.rx_position} and tx_pos: {config.tx_position}"
+    )
+
+    # Save files without ceiling
+    folder_dir = os.path.join(
+        args.output_dir,
+        f"{config.scene_name}",
+        f"idx_{args.index}_rx_pos_{config.rx_position}",
+    )
+    bl_utils.mkdir_with_replacement(folder_dir)
+    bl_utils.save_mitsuba_xml(
+        folder_dir, config.mitsuba_filename, [*devices_names, "Wall", "Floor"]
+    )
+
+    # Save files with ceiling
+    folder_dir = os.path.join(
+        args.output_dir,
+        f"{config.scene_name}",
+        f"ceiling_idx_{args.index}_rx_pos_{config.rx_position}",
+    )
+    bl_utils.mkdir_with_replacement(folder_dir)
+    bl_utils.save_mitsuba_xml(
+        folder_dir,
+        config.mitsuba_filename,
+        [*devices_names, "Wall", "Floor", "Ceiling"],
+    )
+
+
 def main():
     args = create_argparser().parse_args()
     config = bl_utils.make_conf(args.config_file)
