104345ea7cb1de5ad7b63022881aeeb763984a82
HieuLe <hieult1996@gmail.com>
2024-05-20 11:26:45 +0700

    configure lr_schedule

--- sigmap/drl/env_configs/wireless_config.py
+++ sigmap/drl/env_configs/wireless_config.py
@@ -99,11 +99,11 @@ def wireless_config(
         min_lr = max_lr / 50
         return CosineAnnealingWarmupRestarts(
             optimizer,
-            first_cycle_steps=int((total_steps * num_train_steps_per_env_step) // 6),
+            first_cycle_steps=int((total_steps * num_train_steps_per_env_step) // 1),
             cycle_mult=1.0,
             max_lr=max_lr,
             min_lr=min_lr,
-            warmup_steps=int((total_steps * num_train_steps_per_env_step) // 32),
+            warmup_steps=int((total_steps * num_train_steps_per_env_step) // 16),
             gamma=1 / 2,
         )
 
