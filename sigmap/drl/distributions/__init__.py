cf9dca6f9865ec0cf38841b1d44a69144151b719
HieuLe <hieult1996@gmail.com>
2024-06-13 15:49:42 +0700

    use distribution from distrax

---
+++ sigmap/drl/distributions/__init__.py
@@ -0,0 +1,6 @@
+from sigmap.drl.distributions.normal import Normal
+from sigmap.drl.distributions.distribution import Distribution
+from sigmap.drl.distributions.tanh import Tanh
+from sigmap.drl.distributions.bijector import Bijector
+from sigmap.drl.distributions.transformed import Transformed
+from sigmap.drl.distributions.independent import Independent
