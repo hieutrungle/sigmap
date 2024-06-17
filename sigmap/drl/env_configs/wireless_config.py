798630713714b0e7511fe8f7e2f7ab5a87ad5543
HieuLe <hieult1996@gmail.com>
2024-06-17 15:20:14 +0700

    config for jax sac

--- sigmap/drl/env_configs/wireless_config.py
+++ sigmap/drl/env_configs/wireless_config.py
@@ -1,4 +1,4 @@
-from typing import Tuple, Optional
+from typing import Tuple, Optional, Sequence
 import numpy as np
 import torch
 import torch.nn as nn
@@ -16,81 +16,28 @@ def wireless_config(
     args: argparse.Namespace,
     env_name: str,
     exp_name: Optional[str] = None,
-    hidden_size: int = 128,
-    num_layers: int = 3,
-    actor_learning_rate: float = 5e-4,
-    critic_learning_rate: float = 5e-4,
+    replay_buffer_capacity: int = 1000000,
     total_steps: int = 300000,
     random_steps: int = 5000,
     training_starts: int = 10000,
-    batch_size: int = 128,
-    replay_buffer_capacity: int = 1000000,
     ep_len: Optional[int] = None,
-    discount: float = 0.99,
-    use_soft_target_update: bool = False,
-    target_update_period: Optional[int] = None,
-    soft_target_update_rate: Optional[float] = None,
+    # Training settings
+    actor_learning_rate: float = 3e-4,
+    critic_learning_rate: float = 3e-4,
+    alpha_learning_rate: float = 3e-4,
+    batch_size: int = 128,
+    num_train_steps_per_env_step: int = 1,
     # Actor-critic configuration
-    actor_gradient_type="reparametrize",  # One of "reinforce" or "reparametrize"
-    num_actor_samples: int = 1,
-    num_critic_updates: int = 1,
-    # Settings for multiple critics
-    num_critic_networks: int = 1,
-    target_critic_backup_type: str = "mean",  # One of "doubleq", "min", or "mean"
-    # Soft actor-critic
-    backup_entropy: bool = True,
-    use_entropy_bonus: bool = True,
-    temperature: float = 0.1,
-    actor_fixed_std: Optional[float] = None,
-    use_tanh: bool = True,
-    action_scale=0.5,
-    num_train_steps_per_env_step=1,
+    hidden_sizes: Sequence[int] = [128, 128, 128],
+    discount: float = 0.99,
+    tau: float = 0.005,  # soft target update rate
+    num_critics: int = 2,
+    num_critic_updates: int = 5,
+    temperature: float = 0.05,  # temperature for entropy
+    num_actor_samples: int = 5,
+    saved_path: Optional[str] = None,
+    seed: int = 0,
 ):
-    def make_critic(
-        observation_shape: Tuple[int, ...], action_shape: Tuple[int, ...]
-    ) -> nn.Module:
-        ob_dim = np.prod(observation_shape)
-        ac_dim = np.prod(action_shape)
-        return StateActionCritic(
-            ob_dim=ob_dim,
-            ac_dim=ac_dim,
-            n_layers=num_layers,
-            size=hidden_size,
-        )
-
-    def make_actor(
-        observation_shape: Tuple[int, ...], action_shape: Tuple[int, ...]
-    ) -> nn.Module:
-        assert len(observation_shape) == 3
-        ob_dim = np.prod(observation_shape)
-        ac_dim = np.prod(action_shape)
-        if actor_fixed_std is not None:
-            return MLPPolicy(
-                ac_dim=ac_dim,
-                ob_dim=ob_dim,
-                discrete=False,
-                n_layers=num_layers,
-                layer_size=hidden_size,
-                use_tanh=use_tanh,
-                state_dependent_std=False,
-                fixed_std=actor_fixed_std,
-            )
-        else:
-            return MLPPolicy(
-                ac_dim=ac_dim,
-                ob_dim=ob_dim,
-                discrete=False,
-                n_layers=num_layers,
-                layer_size=hidden_size,
-                use_tanh=use_tanh,
-                state_dependent_std=True,
-            )
-
-    def make_actor_optimizer(params: torch.nn.ParameterList) -> torch.optim.Optimizer:
-        return torch.optim.AdamW(params, lr=actor_learning_rate)
-
-    def make_critic_optimizer(params: torch.nn.ParameterList) -> torch.optim.Optimizer:
-        return torch.optim.AdamW(params, lr=critic_learning_rate)
 
     def make_lr_schedule(
         optimizer: torch.optim.Optimizer,
@@ -118,62 +65,37 @@ def wireless_config(
             num_devices=args.num_devices,
             num_tiles_per_device=args.num_tiles_per_device,
             controlled_elements=args.controlled_elements,
-            action_scale=action_scale,
             render_mode="rgb_array" if render else None,
         )
 
-    log_string = "{}_{}_{}_s{}_l{}_alr{}_clr{}_b{}_d{}".format(
+    log_string = "{}_{}_s{}_alr{}_clr{}_alr{}_b{}_d{}".format(
         exp_name or "offpolicy_ac",
         env_name,
-        actor_gradient_type,
-        hidden_size,
-        num_layers,
+        hidden_sizes,
         actor_learning_rate,
         critic_learning_rate,
+        alpha_learning_rate,
         batch_size,
         discount,
     )
 
-    if use_entropy_bonus:
-        log_string += f"_t{temperature}"
-
-    if soft_target_update_rate is not None:
-        log_string += f"_stu{soft_target_update_rate}"
-    else:
-        log_string += f"_htu{target_update_period}"
-
-    if target_critic_backup_type != "mean":
-        log_string += f"_{target_critic_backup_type}"
+    log_string += f"_t{temperature}"
+    log_string += f"_stu{tau}"  # soft_target_update_rate
 
     return {
         "agent_kwargs": {
-            "make_critic": make_critic,
-            "make_critic_optimizer": make_actor_optimizer,
-            "make_critic_schedule": make_lr_schedule,
-            "make_actor": make_actor,
-            "make_actor_optimizer": make_critic_optimizer,
-            "make_actor_schedule": make_lr_schedule,
-            "num_critic_updates": num_critic_updates,
+            "hidden_sizes": hidden_sizes,
+            "actor_learning_rate": actor_learning_rate,
+            "critic_learning_rate": critic_learning_rate,
+            "alpha_learning_rate": alpha_learning_rate,
             "discount": discount,
-            "actor_gradient_type": actor_gradient_type,
-            "num_actor_samples": num_actor_samples,
+            "tau": tau,
+            "num_critics": num_critics,
             "num_critic_updates": num_critic_updates,
-            "num_critic_networks": num_critic_networks,
-            "target_critic_backup_type": target_critic_backup_type,
-            "use_entropy_bonus": use_entropy_bonus,
-            "backup_entropy": backup_entropy,
             "temperature": temperature,
-            "target_update_period": (
-                target_update_period
-                if not (soft_target_update_rate is not None)
-                else None
-            ),
-            "soft_target_update_rate": (
-                soft_target_update_rate
-                if (soft_target_update_rate is not None)
-                else None
-            ),
-            "action_scale": action_scale,
+            "num_actor_samples": num_actor_samples,
+            "saved_path": saved_path,
+            "seed": seed,
         },
         "replay_buffer_capacity": replay_buffer_capacity,
         "log_name": log_string,
