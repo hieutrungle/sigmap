a2ff95513d1dfe4e146eb7e10a407b5bcd40cdb9
HieuLe <hieult1996@gmail.com>
2024-07-17 14:06:51 +0700

    separate blender exec's

--- run_drl_L_hallway_4.sh
+++ run_drl_L_hallway_4.sh
@@ -36,7 +36,7 @@ echo Blender directory: $BLENDER_DIR
 echo Coverage map directory: $SIGMAP_DIR
 echo -e Assets directory: $ASSETS_DIR '\n'
 
-CONFIG_FILE=${SIGMAP_DIR}/config/drl_L_hallway.yaml
+CONFIG_FILE=${SIGMAP_DIR}/config/drl_L_hallway_4.yaml
 
 # Find the blender executable
 for file in ${BLENDER_DIR}/*
@@ -52,7 +52,7 @@ if [ ! -f ${BLENDER_DIR}/addons/mitsuba*.zip ]; then
     wget -P ${BLENDER_DIR}/addons https://github.com/mitsuba-renderer/mitsuba-blender/releases/download/v0.3.0/mitsuba-blender.zip 
     # unzip mitsuba-blender.zip -d ${BLENDER_DIR}/addons
 fi
-${BLENDER_APP} -b ${BLENDER_DIR}/models/hallway_L.blend --python ${SIGMAP_DIR}/sigmap/blender_script/install_mitsuba_addon.py
+${BLENDER_APP} -b ${BLENDER_DIR}/models/hallway_L_4.blend --python ${SIGMAP_DIR}/sigmap/blender_script/install_mitsuba_addon.py
 
 # get scene_name from CONFIG_FILE
 # SCENE_NAME=$(python -c "import yaml; print(yaml.safe_load(open('${CONFIG_FILE}', 'r'))['scene_name'])")
