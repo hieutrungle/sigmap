1a2f967c0e9f6f2de40471ed531bffc2857c7923
HieuLe <hieult1996@gmail.com>
2024-07-17 14:44:46 +0700

    separate blender app

--- run_drl_L_hallway_1.sh
+++ run_drl_L_hallway_1.sh
@@ -39,13 +39,13 @@ echo -e Assets directory: $ASSETS_DIR '\n'
 CONFIG_FILE=${SIGMAP_DIR}/config/drl_L_hallway_1.yaml
 
 # Find the blender executable
-for file in ${BLENDER_DIR}/*
-do
-    if [[ "$file" == *"blender-3.3"* ]];then
-        BLENDER_APP=$file/blender
-    fi
-done
-BLENDER_APP=${BLENDER_DIR}/blender-3.3.14-linux-x64_1
+# for file in ${BLENDER_DIR}/*
+# do
+#     if [[ "$file" == *"blender-3.3"* ]];then
+#         BLENDER_APP=$file/blender
+#     fi
+# done
+BLENDER_APP=${BLENDER_DIR}/blender-3.3.14-linux-x64_1/blender
 
 # Open a random blender file to install and enable the mitsuba plugin
 mkdir -p ${BLENDER_DIR}/addons
