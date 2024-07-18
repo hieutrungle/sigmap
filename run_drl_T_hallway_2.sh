fd25747bc95726df67e44da191554852d29b2277
HieuLe <hieult1996@gmail.com>
2024-07-18 12:16:27 +0700

    install addons

--- run_drl_T_hallway_2.sh
+++ run_drl_T_hallway_2.sh
@@ -54,7 +54,7 @@ if [ ! -f ${BLENDER_DIR}/addons/mitsuba*.zip ]; then
     wget -P ${BLENDER_DIR}/addons https://github.com/mitsuba-renderer/mitsuba-blender/releases/download/v0.3.0/mitsuba-blender.zip 
     # unzip mitsuba-blender.zip -d ${BLENDER_DIR}/addons
 fi
-${BLENDER_APP} -b ${BLENDER_DIR}/models/hallway_T_2.blend --python ${SIGMAP_DIR}/sigmap/blender_script/install_mitsuba_addon.py
+${BLENDER_APP} -b ${BLENDER_DIR}/models/hallway_T_2.blend --python ${SIGMAP_DIR}/sigmap/blender_script/install_mitsuba_addon.py -- --blender_app ${BLENDER_APP}
 
 # get scene_name from CONFIG_FILE
 # SCENE_NAME=$(python -c "import yaml; print(yaml.safe_load(open('${CONFIG_FILE}', 'r'))['scene_name'])")
